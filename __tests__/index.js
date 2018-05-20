b4642da96a4fd5e8ca24183d4a689786fcbbc019
segayuu <segayuu@gmail.com>
2018-05-20 16:21:01 +0900

    [todo]add test render check

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,11 +1,21 @@
 'use strict';
-const webpack = require('webpack');
 const Hexo = require('hexo');
+const renderer = require('../renderer');
+const { join } = require('path');
 
 const hexo = new Hexo(process.cwd(), { silent: true });
 
 beforeAll(async() => {
   await hexo.init();
+  hexo.extend.renderer.register('js', 'js', renderer);
+  hexo.config.webpack = {
+    mode: 'development',
+    entry: 'fixtures/spec_1.js',
+    output: {
+      path: 'dist',
+      filename: 'test-bundle.js'
+    }
+  };
   await hexo.load();
 });
 
@@ -14,9 +24,7 @@ test('todo', () => {
   expect(hexo.render.getOutput('test.js')).toBe('js');
 });
 
-test('todo2', () => {
-  hexo.render.render({
-
-  });
-  expect(hexo.render.getOutput('test.js')).toBe('js');
+test('todo2', async() => {
+  const result = await hexo.render.render({ path: join(__dirname, '..', 'fixtures', 'spec_1.js'), engine: 'js' });
+  console.log(result);
 });
