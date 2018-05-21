7e84c6778ed32d33b5d5c92607f4844147555c24
segayuu <segayuu@gmail.com>
2018-05-21 11:39:03 +0900

    Add Test expect value

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,9 @@
 'use strict';
 const Hexo = require('hexo');
 const renderer = require('../renderer');
+const { readFile } = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(readFile);
 const { join } = require('path');
 
 const hexo = new Hexo(process.cwd(), { silent: true });
@@ -8,23 +11,26 @@ const hexo = new Hexo(process.cwd(), { silent: true });
 beforeAll(async() => {
   await hexo.init();
   hexo.extend.renderer.register('js', 'js', renderer);
-  hexo.config.webpack = {
-    mode: 'development',
-    entry: 'fixtures/spec_1.js',
-    output: {
-      path: 'dist',
-      filename: 'test-bundle.js'
-    }
-  };
-  await hexo.load();
 });
 
-test('todo', () => {
+test('hexo.render - state', () => {
   expect(hexo.render.isRenderable('test.js')).toBe(true);
   expect(hexo.render.getOutput('test.js')).toBe('js');
 });
 
-test('todo2', async() => {
-  const result = await hexo.render.render({ path: join(__dirname, '..', 'fixtures', 'spec_1.js'), engine: 'js' });
-  console.log(result);
+test('webpack mode: development', async() => {
+  const fixtures_path = join(__dirname, '..', 'fixtures');
+  const expectedPromise = readFileAsync(join(fixtures_path, 'result_1.js'), 'utf8');
+
+  hexo.config.webpack = {
+    mode: 'development',
+    entry: 'fixtures/spec_1.js',
+    output: {
+      path: fixtures_path,
+      filename: 'dummy.js'
+    }
+  };
+
+  const result = await hexo.render.render({ path: join(fixtures_path, 'spec_1.js'), engine: 'js' });
+  expect(result).toBe(await expectedPromise);
 });
---
+++ fixtures/result_1.js
@@ -0,0 +1,98 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./fixtures/spec_1.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./fixtures/map.js":
+/*!*************************!*\
+  !*** ./fixtures/map.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nconst hasSynbol = Boolean(Symbol);\n\nconst arrayMap = (array, callback) => {\n  const result = [];\n  let i = -1;\n  const len = array.length;\n  while (++i < len) {\n    result.push(callback(array[i], i));\n  }\n  return result;\n};\n\nconst iterMap = (iterable, callback) => {\n  const result = [];\n  let i = -1;\n  const iter = iterable[Symbol.iterator]();\n  let iterResult;\n  while ((iterResult = iter.next()).done !== false) {\n    result.push(callback(iterResult.value, ++i));\n  }\n  return result;\n};\n\nmodule.exports = (iterable, callback) => {\n  if (Array.isArray(iterable)) {\n    return arrayMap(iterable, callback);\n  } else if (hasSynbol && typeof iterable[Symbol.iterator] === 'function') {\n    return iterMap(iterable, callback);\n  }\n};\n\n\n//# sourceURL=webpack:///./fixtures/map.js?");
+
+/***/ }),
+
+/***/ "./fixtures/spec_1.js":
+/*!****************************!*\
+  !*** ./fixtures/spec_1.js ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\nconst map = __webpack_require__(/*! ./map */ \"./fixtures/map.js\");\n\nmodule.exports = map;\n\n\n//# sourceURL=webpack:///./fixtures/spec_1.js?");
+
+/***/ })
+
+/******/ });
